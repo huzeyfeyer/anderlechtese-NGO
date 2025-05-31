47d7e24cbf44a68dcbb5de4abfd5fa49463eaf9b
huzeyfeyer <huzeyfeyer1@gmail.com>
2025-05-31 21:58:15 +0200

    Initial Spring Boot project

---
+++ src/main/java/be/ehb/huzeyfe_yer/HuzeyfeYerApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.huzeyfe_yer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuzeyfeYerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuzeyfeYerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/huzeyfe_yer/HuzeyfeYerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.huzeyfe_yer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuzeyfeYerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
